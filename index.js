9475b893a1853578dccf3fe771b4826ca6b72f8d
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-18 15:10:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Write your solution in this file
+const employee={
+    employeeName:'sam',
+    streetAddress:'11 Broadway'
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const employeeDetails={...employee}
+    employeeDetails[key]=value
+    return employeeDetails
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const employeeDetails={...employee}
+    delete employeeDetails[key]
+    return employeeDetails;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    // const employeeDetails={...employee}
+    delete employee [key]
+    return employee
+}
